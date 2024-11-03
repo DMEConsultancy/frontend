ecfa45ded4e6ed1c9bdfa928ae12ce258b08fdff
Dropstori <dropstori@James.local>
2024-11-03 03:45:51 +0100

    splash screen commit

---
+++ src/splash.jsx
@@ -0,0 +1,21 @@
+export default function Splash(){
+    return(
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
